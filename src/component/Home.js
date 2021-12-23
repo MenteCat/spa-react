d1dd0a28d0b8ceafbcc01dbbb0583d255a980ce5
MenteCat <tinaiuso@gmail.com>
2021-12-23 12:04:57 +0000

    home added

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Home=()=> {
     return(
         <>
-        <h1>Home page</h1>
+        <div className='mainSection'>
+            <div className='contentBox'>
+                <h1>She sights a Bird, she chuckles</h1>
+                <p>
+                She sights a Bird, she chuckles <br></br>
+                She flattens then she crawls <br></br>
+                She runs without the look of feet <br></br>
+                Her eyes increase to Balls … <br></br>
+                </p>
+                <div className='btnBox'>
+                    <div className='btn'>
+                    <NavLink to='/about' className='readMore'
+                    >Read More</NavLink>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+        </div>
         </>
 
     )
