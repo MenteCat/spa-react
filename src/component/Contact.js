94fb4a034ca817a742c20b20ef33fb13c5bbc8cd
Tina Iuso <tinaiuso@gmail.com>
2021-12-31 12:29:49 +0000

    completed SPA but some style tweaks needed

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,9 +1,34 @@
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
+import '../css/ContactStyle.css';
 const Contact=()=> {
     return(
         <>
-        <h1>Contact page</h1>
+        <h1>Contact us</h1>
+        <form className='contact'>
+        <div>
+            <label htmlFor='name'>Name</label>
+            <input type="text" placeholder='Enter your name'></input>
+        </div>
+        <div>
+            <label htmlFor='name'>Email</label>
+            <input type="email" placeholder='Enter your email'></input>
+        </div>
+        <div>
+            <label htmlFor='password'>Password</label>
+            <input type="password" placeholder='Enter your password'></input>
+        </div>
+        <div>
+            <label htmlFor='mobile'>Mobile</label>
+            <input type="mobile" placeholder='Enter your mobile number'></input>
+        </div>
+        <div className='btnBox'>
+                    <div className='btn'>
+                    <NavLink to='/' className='readMore'
+                    >Submit Details</NavLink>
+                    </div>
+        </div>
+        </form>
         </>
 
     )
--- src/component/Policy.js
+++ src/component/Policy.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import '../css/PolicyStyle.css';
 
 const Policy=()=> {
     return(
         <>
-        <h1>Policy page</h1>
+        <h1>Adoption Policy</h1>
+        <div className='policy'>
+            <ol>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Molestie ac feugiat sed lectus vestibulum.
+                </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Molestie ac feugiat sed lectus vestibulum.
+                </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Molestie ac feugiat sed lectus vestibulum.
+                </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Molestie ac feugiat sed lectus vestibulum.
+                </li>
+                <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                    Molestie ac feugiat sed lectus vestibulum.
+                </li>
+
+            </ol>
+        </div>
         </>
 
     )
